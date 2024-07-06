2880b5364bd7eff4ca37b8836fa66e7c316963c5
Pierre Westphal <pierre.westphal@outlook.com>
2024-07-05 21:29:23 -0300

    add validation fields for the user creation

--- api/main.py
+++ api/main.py
@@ -1,14 +1,17 @@
 from schemas.user import User
 from models.user_model import UserModel
-from typing import Annotated, Union
+from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
 from config.database_connection import engine, SessionLocal
 from models.base_model import Base
-from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import Request, Response, Depends
 from config.config import AppConfig
+from fastapi.exceptions import RequestValidationError
+
 
 app = FastAPI()
 
@@ -33,6 +36,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    error_message = {"detail": "Validation Error", "error": exc.errors()}
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder(error_message),
+    )
+
 
 @app.get("/")
 def root():
@@ -62,10 +74,10 @@ async def create_user(
     Returns:
         Union[User, Response]: The created user data if successful, otherwise a Response object.
     """
-    user = UserModel(**user_data.dict())  # Create a new UserModel instance
     try:
+        user = UserModel(**user_data.dict())  # Create a new UserModel instance
         db.add(user)  # Add the user to the session
         db.commit()  # Commit the changes to the database
         return user_data  # Return the created user data
     except Exception as exc:
-        return Response  # Return a Response object in case of an error
+        raise HTTPException(status_code=400, detail="Price must be non-negative")
--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic.functional_validators import field_validator
 
 class User(BaseModel):
     first_name:str
@@ -6,3 +7,21 @@ class User(BaseModel):
     username:str
     email:str
     password:str
+ 
+    @field_validator("password")
+    def validate_password(cls, v):
+        if len(v) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        return v
+    
+    @field_validator("username")
+    def validate_username(cls, v):
+        if len(v) < 4:
+            raise ValueError("Username must be at least 4 characters long")
+        return v
+    
+    @field_validator("email")
+    def validate_email(cls, v):
+        if "@" not in v:
+            raise ValueError("Invalid email address")
+        return v
